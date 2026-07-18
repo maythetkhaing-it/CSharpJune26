19b8b9d3c8e8db74197f64266ee35e3ac5a9ea33
May Thet(MayCoderMM) <maycodermm@gmail.com>
2026-07-18 17:29:32 +0630

    feat : adding user table

---
+++ ClassLibrary.Database/AppDbContextModels/TblUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary.Database.AppDbContextModels;
+
+public partial class TblUser
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string UserPassword { get; set; } = null!;
+}
